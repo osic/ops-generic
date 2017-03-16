1c89c63d4e97da6088e739e669eb9699cf161cf3
Nish Patwa <nishpatwa007@gmail.com>
2017-03-15 23:43:51 -0500

    Changed python files for pep8 compliant

--- tools/vms_for_host/vms_for_host.py
+++ tools/vms_for_host/vms_for_host.py
@@ -10,8 +10,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 #
-# This tool is used to get the list of Virtual Machine on particular compute host
-# It prints the List of VMs with their owner's user ID
+# This tool is used to get the list of Virtual Machine on particular
+# compute host  It prints the List of VMs with their owner's user ID
 #
 # USAGE
 # python VMSforHost.py --host <FQDN of Compute Host>
@@ -26,59 +26,67 @@ from nova_client import NovaClient
 from keystone_client import KeystoneClient
 from authentication import Authentication
 
+
 class ChildArgParser(argparse.ArgumentParser):
-      """
-      A class that Inherits the class ArgumentParser
-      """
-      def validate(self,args):
+    """
+    A class that Inherits the class ArgumentParser
+    """
+
+    def validate(self, args):
         """
         A method that validates the arguments.
         """
-        if not '.' in args.host:
-          print "Please call with the FQDN of the host"
-          sys.exit(1)
+        if '.' not in args.host:
+            print "Please call with the FQDN of the host"
+            sys.exit(1)
+
 
 class VMsforHost(object):
-      """
-      A base class for the tool.
-      """
-      def __init__(self):
-          """
-          Constructor that validates arguments and setup necessary clients needed for the tool
-          """
-          self.check_arguments()
-          self.setup_clients()
+    """
+    A base class for the tool.
+    """
 
-      def check_arguments(self):
-          """
-          A method to validate arguments - Called by Constructor
-          """
-          parser = ChildArgParser()
-          parser.add_argument("--host", required=True, help='FQDN required!')
-          self.args=parser.parse_args()
-          parser.validate(self.args)
+    def __init__(self):
+        """
+        Constructor that validates arguments and setup necessary
+        clients needed for the tool
+        """
+        self.check_arguments()
+        self.setup_clients()
 
-      def setup_clients(self):
-          """
-          A method to seting up authentication and necessary clients for the tool - Called by Constructor
-          """
-          auth = Authentication()
-          self.novaclient = NovaClient(auth)
-          self.keystoneclient = KeystoneClient(auth)
+    def check_arguments(self):
+        """
+        A method to validate arguments - Called by Constructor
+        """
+        parser = ChildArgParser()
+        parser.add_argument("--host", required=True, help='FQDN required!')
+        self.args = parser.parse_args()
+        parser.validate(self.args)
 
-      def print_vms_for_host(self):
-          """
-          A method to print the VMs specific to the compute host
-          """
-          vms = self.novaclient.get_vms_for_host(self.args.host)
-          users = self.keystoneclient.get_user_dict()
-          for vm in vms:
-                print "%-45s: %-15s" % (vm.name, users[vm.user_id].name)
+    def setup_clients(self):
+        """
+        A method to seting up authentication and necessary
+        clients for the tool - Called by Constructor
+        """
+        auth = Authentication()
+        self.novaclient = NovaClient(auth)
+        self.keystoneclient = KeystoneClient(auth)
+
+    def print_vms_for_host(self):
+        """
+        A method to print the VMs specific to the compute host
+        """
+        vms = self.novaclient.get_vms_for_host(self.args.host)
+        users = self.keystoneclient.get_user_dict()
+        for vm in vms:
+            print "%-45s: %-15s" % (vm.name, users[vm.user_id].name)
+
 
 def main():
 
     vmsforhost = VMsforHost()
     vmsforhost.print_vms_for_host()
+
 
 if __name__ == "__main__":
 
